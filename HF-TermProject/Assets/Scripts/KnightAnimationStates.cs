4bc89ce7c5d2103569be0082677b7ee00ae1fcc2
hafsaxfarid <hafsaxfarid@gmail.com>
2023-02-02 21:52:24 -0500

    Player Animations
    Knight animations created. Knight animation states + behaviour script created.

---
+++ HF-TermProject/Assets/Scripts/KnightAnimationStates.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum KnightAnimationStates
+{
+    KNIGHT_IDLE,                // 0
+    KNIGHT_WALK,                // 1
+    KNIGHT_RUN,                 // 2
+    KNIGHT_JUMP,                // 3
+    KNIGHT_ATTACK,              // 4
+    KNIGHT_HURT,                // 5
+    KNIGHT_DIE,                 // 6
+    KNIGHT_TOTAL_ANIMATIONS     // 7
+}
---
+++ HF-TermProject/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void KnightMove()
+    {
+
+    }
+
+    private void KnightJump()
+    {
+
+    }
+
+    private void KnightAttack()
+    {
+
+    }
+
+    private void KnightHurt()
+    {
+
+    }
+
+    private void KnightDie()
+    {
+
+    }
+}
